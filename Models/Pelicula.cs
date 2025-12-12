f78d0556e0c0d85b02caef6eed50d8e87d79ae50
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-12-12 02:00:46 -0600

    Creación de algunas verificaciones

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -1,26 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace Filmera.Models;
 
 public partial class Pelicula
 {
+    // ID
+    [Required]
     public string IdPelicula { get; set; } = null!;
 
+    // Titulo
+    [Required]
     public string? Titulo { get; set; }
 
+    // Sinopsis / Trama
+    [Required]
     public string? Sinopsis { get; set; }
 
+    // Director
+    [Required]
     public string? Director { get; set; }
 
+    // Genero
+    [Required]
     public string? Genero { get; set; }
 
+    // Duracion
+    [Required]
     public int? Duracion { get; set; }
 
+    // Calificacion
+    [Required]
     public int? Calificacion { get; set; }
 
+    // Año de lanzamiento
+    [Required]
     public int? Lanzamiento { get; set; }
 
+    // Público objetivo
+    [Required]
     public string? Publico { get; set; }
 }
